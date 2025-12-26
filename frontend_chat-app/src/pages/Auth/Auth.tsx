c139b9f02f7f27f61992f616e10bba0a4bc796eb
fardin-Sachi <fardin2002@cseku.ac.bd>
2025-12-26 23:48:14 +0600

    Add Web Logo

--- frontend_chat-app/src/pages/Auth/Auth.tsx
+++ frontend_chat-app/src/pages/Auth/Auth.tsx
@@ -11,12 +11,13 @@ const Auth: React.FC = () => {
             <div className="w-full md:w-6/12 bg-sky-500 p-8 text-white flex flex-col justify-center">
                 <div className="text-center mb-8">
                     <div className="flex justify-center mb-6">
-                        <div className="bg-white/20 p-4 rounded-full">
-                            <Mail className="size-10" />
+                        <div className="">
+                            {/* <Mail className="size-10" /> */}
+                            <img src="Screenshot from 2025-12-26 23-45-33-modified.png" alt="Onyx" className="size-30"></img>
                         </div>
                     </div>
-                    <h1 className="text-3xl font-bold mb-2">Welcome to Chatty</h1>
-                    <p>Connect with friends and family, anytime and anywhere</p>
+                    <h1 className="text-3xl font-bold mb-2">Welcome to Onyx</h1>
+                    <p>Where Every Connection Counts</p>
                 </div>
                 <div className="mt-10 text-center">
                     <p className="text-sm opacity-80">Join thousands of users today!</p>
